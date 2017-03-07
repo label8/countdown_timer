a1e8d712c3d365c38be6349635ead3b1ed7142d7
Tsunemasa Hachiya <label8.me@gmail.com>
2017-03-07 20:28:21 +0900

    Add validation logic and change format of time

--- js/timer.js
+++ js/timer.js
@@ -7,22 +7,40 @@ var startDate;
 var stopDate;
 
 // 定数値(秒)
-var SIGNAL_CNT = 4;
+var SIGNAL_CNT = 4; // 開始のカウント(秒)(指定秒 + 1を指定)
 
-var HOUR  = 0;
-var MIN   = 0;
-var SEC   = 15;
-var MILLI = 1000; // ミリ秒 (1000ミリ秒 = 1秒)
+var HOUR = 1;    // タイマー時
+var MIN  = 4;    // タイマー分
+var SEC  = 60;   // タイマー秒
+var MSEC = 1000; // ミリ秒 (1000ミリ秒 = 1秒)
+var WARN = 5;    // 残り時間が少ない警告(秒)
 
 var isNext = 0;
 
+function validTimer(){
+    if (HOUR > 60 || HOUR < 0){
+        alert("HOURの設定範囲が間違っています");
+        return false;
+    }
+    if (MIN > 60 || MIN < 0){
+        alert("MINの設定範囲が間違っています");
+        return false;
+    }
+    if (SEC > 60 || SEC < 0){
+        alert("SECの設定範囲が間違っています");
+        return false;
+    }
+
+    return true;
+}
+
 // 設定時間をミリ秒に変換
 function setup(){
-    signalTime = SIGNAL_CNT * MILLI;
+    signalTime = SIGNAL_CNT * MSEC;
 
-    var h = HOUR * 60 * 60 * MILLI;
-    var m = MIN * 60 * MILLI;
-    var s = SEC * MILLI;
+    var h = HOUR * 60 * 60 * MSEC;
+    var m = MIN * 60 * MSEC;
+    var s = SEC * MSEC;
     finishTime = h + m + s;
 }
 
@@ -83,35 +101,52 @@ function zeroPadding(num, len){
 
 // 開始合図のタイマーを表示
 function displaySignalTimer(time){
-    var sec = Math.floor(time / MILLI);
+    var sec = Math.floor(time / MSEC);
+
+    var timerStr = "";
     if (sec <= 0){
-        var str = "Go!"
-        $("#start_timer").text(str);
+        timerStr = "Go!"
     } else {
-        $("#start_timer").text(sec);
+        timerStr = String(sec);
     }
+
+    $("#start_timer").text(timerStr);
 }
 
 // 終了タイマーを表示
 function displayFinishTimer(time){
     // タイムスタンプを時分秒に切り出す
-    var hour  = Math.floor(time / (60 * 60 * MILLI));
-    var min   = Math.floor(time / (60 * MILLI));
-    var sec   = Math.floor(time / MILLI);
-    // ミリ秒を取得
-    var milli = time % MILLI;
-    var milli = String(milli).substr(0, 2);
+    var hour  = Math.floor(time / (60 * 60 * MSEC));
+    var hTime = time % (60 * 60 * MSEC);
 
-    // 1桁なら頭にゼロを付ける
-    hour  = zeroPadding(hour, 2);
-    min   = zeroPadding(min, 2);
-    sec   = zeroPadding(sec, 2);
-    milli = zeroPadding(milli, 2);
+    var min   = Math.floor(hTime / (60 * MSEC));
+    var mTime = time % (60 * MSEC);
 
-    $("#show_timer").text(hour + ":" + min + ":" + sec + ":" + milli);
+    var sec  = Math.floor(mTime / MSEC);
+
+    var msec = time % MSEC;
+    var sumTime = hour + min + sec + msec;
+
+    var timerStr = "";
+    if (sumTime <= 0){
+        timerStr = "Time out..."
+    } else {
+        // ミリ秒を切り詰め
+        msec = String(msec).substr(0, 2);
+        // 1桁なら頭にゼロを付ける
+        h  = zeroPadding(hour, 2);
+        m  = zeroPadding(min, 2);
+        s  = zeroPadding(sec, 2);
+        ms = zeroPadding(msec, 2);
+        timerStr = h + ":" + m + ":" + s + ":" + ms;
+    }
+
+    $("#show_timer").text(timerStr);
 }
 
 $(function(){
+    var res = validTimer();
+    if (!res) return false;
     setup();
     start();
 });
